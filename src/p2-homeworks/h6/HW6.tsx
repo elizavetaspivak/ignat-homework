24096e5f7dd4c54feb53496b90ce0d9ee92b5a5f
elizavetaspivak <elizazbethspivak99@gmail.com>
2021-04-19 21:37:21 +0300

    HW 6 and 7

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -10,6 +10,7 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
+        setValue(restoreState<string>('editable-span-value',''))
         // setValue()
     }
 
